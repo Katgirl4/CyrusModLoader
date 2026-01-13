7e1a0d01acdab1f982e3a081b719f0a9e290d045
a3oYzmN4fGhm7rA4ob <15acresofbrokenglass@proton.me>
2026-01-12 20:45:23 -0800

    work on GUI mod manager

--- linux/modManagerLinux.py
+++ linux/modManagerLinux.py
@@ -3,15 +3,61 @@ import json, gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+# Open config file when application starts.
+    # If config is found, open it. If config is not found, then create a new file and try to open it. (TODO: make it fill blank fields)
+config = None
+while not config:
+    try:
+        config = open("cfg.json", 'r')
+        try:
+            cfg = json.load(config)
+        except(json.JSONDecodeError):
+            config.close()
+            config = open('cfg.json', 'w')
+            json.dump({"None" : "None"}, config)
+            config.close()
+            config = open("cfg.json", 'r')
+            cfg = json.load(config)
+    except(FileNotFoundError):
+        config = open('cfg.json', 'w')
+        json.dump({"None" : "None"}, config)
+        config.close()
+
 class MainWindow(Gtk.Window):
 
     def __init__(self):
-        super().__init__(title="CyrusModManager")
-        self.set_default_size(400, 300)
         
-        button = Gtk.Button(label="Test")
-        button.connect("clicked", self.onTest)
-        self.add(button)
+
+        super().__init__(title="CyrusModManager")
+        self.set_border_width(5)
+
+        outerBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+    
+        directoryBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        directoryBox.set_homogeneous(False)
+
+        modsBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        modsBox.set_homogeneous(False)
+        
+        outerBox.pack_start(directoryBox, True, True, 0)
+        outerBox.pack_start(modsBox, True, True, 0)
+
+        gameDirectoryLabel = Gtk.Label()
+        gameDirectoryLabel.set_text("Game Directory Configuration")
+        gameDirectoryLabel.set_justify(Gtk.Justification.LEFT)
+        directoryBox.pack_start(gameDirectoryLabel, True, True, 0)
+
+        gameDirectorySelection = Gtk.FileChooserButton()
+        gameDirectorySelection.set_homogeneous(False)
+        modsBox.pack_start(gameDirectorySelection, True, True, 0)
+
+        modsListLabel = Gtk.Label()
+        modsListLabel.set_text("Mods List (TODO)")
+        gameDirectoryLabel.set_justify(Gtk.Justification.LEFT)
+        modsBox.pack_start(modsListLabel, True, True, 0)
+
+        self.add(outerBox)
+        
 
     def onTest(self, widget):
         print("Test")
@@ -20,14 +66,7 @@ class MainWindow(Gtk.Window):
 
 
 
-# Open config file when application starts.
-    # If config is found, open it. If config is not found, then create a new file and try to open it. (TODO: make it fill blank fields)
-config = None
-while not config:
-    try:
-        config = open("cfg.json", "r")
-    except(FileNotFoundError):
-        config = open('cfg.json', 'w')
+
 
 window = MainWindow()
 window.connect("destroy", Gtk.main_quit)
