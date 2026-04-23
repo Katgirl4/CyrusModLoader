57951a2a6e07817c59b3a47f8bc5273873947c85
a3oYzmN4fGhm7rA4ob <15acresofbrokenglass@proton.me>
2026-04-23 13:16:55 -0700

    work on installer and other functions, added OS check

--- loader/modManagerCLI.py
+++ loader/modManagerCLI.py
@@ -1,4 +1,4 @@
-import json, sys, os, subprocess, re, requests, string, time, threading, climage
+import json, sys, os, subprocess, re, requests, string, time, threading, climage, platform
 from colorama import *
 from bs4 import *
 
@@ -55,6 +55,8 @@ def printMessage(text, style):
             print(Fore.BLUE + "INFO\t" + Fore.WHITE + f"{text}")
         case "log":
             print(Fore.GREEN + "LOG\t" + Fore.WHITE + f"{text}")
+        case "fuck":
+            print(Fore.MAGENTA + "CRTCL\t" + Fore.YELLOW + f"{text}" + Fore.WHITE)
         case _:
             sys.exit()
 
@@ -94,12 +96,19 @@ def toggleMod(modName):
 def setup():
     pass
 
-def install():
-    pass
+def getOS():
+    match(platform.uname()):
+        case 'Linux':
+            return 'linux'
+        case 'Windows':
+            return 'microslop'
+        case _:
+            return 'other'
 
 def main():
-    print(climage.convert('../assets/loader/CMLLogo.png', width=40))
-    printMessage("CyrusModLoader command line tool release NULL", "info")
+    #print(climage.convert('../assets/loader/CMLLogo.png', width=40))
+    print("\t\t\t\tPLACEHOLDER MODLOADER LOGO HERE")
+    printMessage("CyrusModLoader release NULL", "info")
     printMessage("Type \"help\" for a list of commands.", "info")
 
     # Open config file when application starts.
@@ -109,23 +118,77 @@ def main():
     global cfg
     config, cfg = getConfig()
 
+    # Check OS first
+    operatingSystem = getOS()
+    if getOS == 'other':
+        printMessage(f"Your operating system \'{platform.uname()}\', is not supported.")
+        sys.exit()
+    
+
+    # Make sure that config has all the correct fields
+    while (not ('gameDirectoryString' in cfg)):
+        printMessage("Malformed configuration file detected. Configuration will be reset to defaults.", 'err')
+        resetConfig()
+        config, cfg = getConfig()
+
+
     checkGameExists()
     checkFolderModsExist()
     checkFolderDisabledExist()
 
     while True:
-        userInput = input(Fore.CYAN + "\nCML" "> " + Fore.WHITE)
+        userInput = input(Fore.CYAN + "\nCML> " + Fore.WHITE)
 
         # Intercept single word commands
         match (userInput):
             case "exit":
+                printMessage("Exit from program requested, killing CML...", 'fuck')
+                time.sleep(0.1)
                 sys.exit()
+                
             case "list":
                 listMods()
             case "help":
                 print("help text todo later")
-            case "install":
-                print("bepinex automated installer placeholder")
+            case "install": # TODO on getting this working
+                if checkGameExists():
+                    printMessage("Checks passed, GO for install.", 'info')
+                    printMessage("HTTP GET request to \'https://github.com/BepInEx/BepInEx/\'.", 'log')
+                    result = requests.get("https://github.com/BepInEx/BepInEx/")
+
+                    printMessage("Scraping latest release version information.", 'log')
+                    soup = BeautifulSoup(result.content, 'html.parser')
+                    latestRelease = soup.find_all('span', class_='css-truncate css-truncate-target text-bold mr-2')
+                    if latestRelease:
+                        for item in latestRelease:
+                            # print(f"Latest release: {item.get_text()}")
+
+                            # Now, use that to find the latest release of BepinEx and download it.
+                            versionData_A = re.search(r"((?:[0-9]+\.)+[0-9]+)", item.get_text())
+                            if versionData_A:
+                                
+                                printMessage(f"Latest version identified as {versionData_A.group(1)}.", 'log')
+                                # TODO: fix download url, i think i need the winblows one since it will run under proton
+                                downloadURL = f"https://github.com/BepinEx/BepinEx/releases/download/{"v" + str(versionData_A.group(1))}/BepInEx_linux_x64_{str(versionData_A.group(1))}.zip"
+
+                                printMessage(f"HTTP GET request to \'{downloadURL}\'.", 'log')
+                                printMessage("This might take a moment depending on your internet connection, please be patient.", 'info')
+                                download = requests.get(downloadURL, stream=True)
+
+                                with open("bepinex_latest.zip", "wb") as bpxZip:
+                                    for chunk in download.iter_content(chunk_size=8192):
+                                        if chunk:
+                                            bpxZip.write(chunk)
+                                
+                                printMessage("Download complete. Saved to bepinex_latest.zip.", 'log')
+
+                                # TODO: move file and extract it
+
+                            else:
+                                printMessage("Regex that should never fail has failed to parse version data out of GET request. Something has gone terribly wrong.", 'err')        
+                else:
+                    printMessage(f"Contract Rush DX.exe not found in game directory {cfg['gameDirectoryString']}. Install cannot proceed unless game is present in game directory.")
+            
             case "setup":
                 print("setup folders")
                 if os.path.isfile(f"{cfg['gameDirectoryString']}Contract Rush DX.exe"):
